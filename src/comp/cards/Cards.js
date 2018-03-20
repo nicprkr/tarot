62d9d099943c8a0bd8ae1f8febeec47d816166b2
evelyn <evelyncranston@Live.ca>
2018-03-20 16:00:50 -0700

    Trying to add flip

--- src/comp/cards/Cards.js
+++ src/comp/cards/Cards.js
@@ -21,134 +21,156 @@ class Cards extends Component {
         {
             name: "The Fool",
             number: 0,
-            meaning: "Let go of expectations and trust your instincts."
+            meaning: "Let go of expectations and trust your instincts.",
+            flipped: false
 
         }, 
           
         {
             name: "The Magician",
             number: 1,
-            meaning: "Be creative and stay open -- your possibilities are endless."
+            meaning: "Be creative and stay open -- your possibilities are endless.",
+            flipped: false
         },
             
         {
             name: "The High Priestess",
             number: 2,
-            meaning: "Get your self out of the way and become attuned to a more spiritual view."
+            meaning: "Get your self out of the way and become attuned to a more spiritual view.",
+            flipped: false
         },
             
         {
             name: "The Empress",
             number: 3,
-            meaning: "Look for opportunities to be generous, warm, and nurturing."
+            meaning: "Look for opportunities to be generous, warm, and nurturing.",
+            flipped: false
         },
             
         {
             name: "The Emperor",
             number: 4,
-            meaning: "You have the wisdom and authority to achieve your goals."
+            meaning: "You have the wisdom and authority to achieve your goals.",
+            flipped: false
         },
             
         {
             name: "The Hierophant",
             number: 5,
-            meaning: "Expand your inner knowledge and share it with others."
+            meaning: "Expand your inner knowledge and share it with others.",
+            flipped: false
         },
             
         {
             name: "The Lovers",
             number: 6,
-            meaning: "Integrate two potential realities or let go of one of them."
+            meaning: "Integrate two potential realities or let go of one of them.",
+            flipped: false
         },
             
         {
             name: "The Chariot",
             number: 7,
-            meaning: "Use your powers of freedom and flexibility to drive your future."
+            meaning: "Use your powers of freedom and flexibility to drive your future.",
+            flipped: false
         },
             
         {
             name: "Strength",
             number: 8,
-            meaning: "Balance your primal force with intuition and compassion."
+            meaning: "Balance your primal force with intuition and compassion.",
+            flipped: false
         },
             
         {
             name: "The Hermit",
             number: 9,
-            meaning: "Gaze into the mysteries of your inner life."
+            meaning: "Gaze into the mysteries of your inner life.",
+            flipped: false
         },
             
          {
             name: "Wheel of Fortune",
             number: 10,
-            meaning: "Learn to go with the flow without resisting ups and downs."
+            meaning: "Learn to go with the flow without resisting ups and downs.",
+             flipped: false
         },
             
         {
             name: "Justice",
             number: 11,
-            meaning: "Balance the scales to support truth and serve the greatest good."
+            meaning: "Balance the scales to support truth and serve the greatest good.",
+            flipped: false
         },
             
          {
             name: "The Hanged Man",
             number: 12,
-            meaning: "You have reached a crossroads, making it possible to clear the slate and start over."
+            meaning: "You have reached a crossroads, making it possible to clear the slate and start over.",
+             flipped: false
         },
             
         {
             name: "Death",
             number: 13,
-            meaning: "Shed the old to make room for the new."
+            meaning: "Shed the old to make room for the new.",
+            flipped: false
         },
             
          {
             name: "Temperance",
             number: 14,
-            meaning: "The time is now for self-healing."
+            meaning: "The time is now for self-healing.",
+             flipped: false
         },
             
          {
             name: "The Devil",
             number: 15,
-            meaning: "Put subtlety aside and empower your passion and confidence."
+            meaning: "Put subtlety aside and empower your passion and confidence.",
+             flipped: false
         },
             
          {
             name: "The Tower",
             number: 16,
-            meaning: "Because of circumstances beyond your control, you have no choice."
+            meaning: "Because of circumstances beyond your control, you have no choice.",
+             flipped: false
         },
             
         {
             name: "The Star",
             number: 17,
-            meaning: "Rise above your day-to-day and connect with the divine."
+            meaning: "Rise above your day-to-day and connect with the divine.",
+            flipped: false
         },
         
          {
             name: "The Moon",
             number: 18,
-            meaning: "This is an opportunity to draw messages from your inner self."
+            meaning: "This is an opportunity to draw messages from your inner self.",
+             flipped: false
         },
             
          {
             name: "The Sun",
             number: 19,
-            meaning: "You are in the right place at the right time."
+            meaning: "You are in the right place at the right time.",
+             flipped: false
         },
             
          {
             name: "Judgement",
             number: 20,
-            meaning: "You have the power to make changes and feel complete."
+            meaning: "You have the power to make changes and feel complete.",
+             flipped: false
         },
             
         {
             name: "The World",
             number: 21,
-            meaning: "You are in a timeless state of grace where all is well."
+            meaning: "You are in a timeless state of grace where all is well.",
+            flipped: false
         }]
         }
     }
@@ -163,9 +185,8 @@ class Cards extends Component {
         });
     }
     
-    addToHand(obj){
+        addToHand(obj){
         var tempArr = this.state.hand;
-        tempArr.push(obj);
         
         this.setState({
             hand: tempArr
