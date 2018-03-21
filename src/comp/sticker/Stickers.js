858ead5bf2c929974d6a4a9bad95868a4df2f50d
evelyn <evelyncranston@Live.ca>
2018-03-20 23:01:51 -0700

    Sticker sizing

--- src/comp/sticker/Stickers.js
+++ src/comp/sticker/Stickers.js
@@ -13,14 +13,14 @@ class Stickers extends Component {
             showDisplay:false,
             stickers:[],
             baseStickers: {
-                    candle: {src: require("../../imgs/candle.png"), height: "100px"},
+                    candle: {src: require("../../imgs/candle.png"), height: "110px"},
                     singingbowl: {src: require("../../imgs/singingbowl.png"), height: "50px"},
-                    pentagram: {src: require("../../imgs/pentagram.png"), height: "50px"},
+                    pentagram: {src: require("../../imgs/pentagram.png"), height: "100px"},
                     quartz: {src: require("../../imgs/quartz.png"), height: "50px"},
-                    chalice: {src: require("../../imgs/chalice.png"), height: "50px"},
+                    chalice: {src: require("../../imgs/chalice.png"), height: "75px"},
                     dragoncup: {src: require("../../imgs/dragoncup.png"), height: "75px"},
-                    saltlamp: {src: require("../../imgs/saltlamp.png"), height: "50px"},
-                    tapestry: {src: require("../../imgs/tapestry.png"), height: "50px"}
+                    saltlamp: {src: require("../../imgs/saltlamp.png"), height: "80px"},
+                    tapestry: {src: require("../../imgs/tapestry.png"), height: "200px"}
                 }
         }
         
@@ -57,17 +57,16 @@ class Stickers extends Component {
                     x:(ev.pageX-90),
                     y:(ev.pageY-330),
                     id:this.state.myId,
-                    src:this.refs["u"+this.state.myId].src,
-                    height:this.refs["u"+this.state.myId].height
+                    src:this.refs["u"+this.state.myId].src
                 })
             });
             
             this.refs.thedisplay.addEventListener("click", (ev)=>{
+                console.log(this.refs["u"+this.state.myId]);
                 this.socket.emit("stick", {
                     x:(ev.pageX-90),
                     y:(ev.pageY-330),
-                    src:this.refs["u"+this.state.myId].src,
-                    height:this.refs["u"+this.state.myId].height
+                    src:this.refs["u"+this.state.myId].src
                 });
             });
 
@@ -80,19 +79,34 @@ class Stickers extends Component {
             });
         });
         
-//        this.socket.on("newmove", (data)=>{
-//            //console.log(data);
-//            this.refs["u"+data.id].style.left = data.x+"px";
-//            this.refs["u"+data.id].style.top = data.y+"px";
-//            this.refs["u"+data.id].src = data.src;
-//            
-//        });
+        this.socket.on("newmove", (data)=>{
+            //console.log(data);
+            this.refs["u"+data.id].style.left = data.x+"px";
+            this.refs["u"+data.id].style.top = data.y+"px";
+            this.refs["u"+data.id].src = data.src;
+           
+            
+        });
         
     }
     
+    getHeight(src){
+       
+        var array = Object.values(this.state.baseStickers);
+        if(src){
+            for(var i = 0; i < array.length; i ++){
+                if(src.includes(array[i].src)){
+                    return(array[i].height);
+                }
+            }
+        }
+    }
+    
+    
     handleImage(evt){
+        
         this.refs["u"+this.state.myId].src = evt.target.src;
-        this.refs["u"+this.state.myId].height = evt.target.trueheight;
+        console.log(this.getHeight(evt.target.src));
     }
     
     handleDisplay(roomString){
@@ -107,14 +121,14 @@ class Stickers extends Component {
         
         var allimgs = this.state.allusers.map((obj, i)=>{
             return (
-                <img ref={"u"+obj} className="allImgs" src={this.state.myImg} height={obj.height} key={i} />
+                <img ref={"u"+obj} className="allImgs" src={this.state.myImg} height={"100px"} key={i} />
             )    
         });
         
         var allstickers = this.state.stickers.map((obj, i)=>{
             var mstyle = {left:obj.x, top:obj.y};
             return (
-                <img style={mstyle} key={i} src={obj.src} height={obj.height} className="allImgs" />
+                <img style={mstyle} key={i} src={obj.src} height={this.getHeight(obj.src)} className="allImgs" />
             )
         })
         
@@ -130,24 +144,31 @@ class Stickers extends Component {
             //Display
             comp = (
                 <div>
+                    <Row>
+                        <Col xs="12">
+                            <div ref="thedisplay" id="display">
+                                {allimgs}
+                                {allstickers}
+                                <div id="altarTable">
+                                    <img style={{marginTop:"200px", maxHeight:"300px", marginLeft:"auto", marginRight:"auto"}} height="400px" src={require("../../imgs/table.png")}/> 
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
                 
-                    <div ref="thedisplay" id="display">
-                        {allimgs}
-                        {allstickers}
-                        <div id="altarTable">
-                            <img style={{paddingTop:"100px"}} height="400px" src={require("../../imgs/table.png")}/> 
-                        </div>
-                    </div>
-                
-                    <div id="controls">
-                        <div id="stickerChoice" style={{flexDirection:"column"}}>
-                            {Object.values(this.state.baseStickers).map((obj, i)=>{
-                                return (
-                                    <img onClick={this.handleImage} key={i} src={obj.src} height="100px" trueheight={obj.height}/>
-                                )
-                            })}
-                        </div>
-                    </div>
+                    <Row>
+                        <Col xs="12">
+                            <div id="controls">
+                                <div id="stickerChoice" style={{flexDirection:"column"}}>
+                                    {Object.values(this.state.baseStickers).map((obj, i)=>{
+                                        return (
+                                            <img onClick={this.handleImage} key={i} src={obj.src} height="100px"/>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
                 </div>
             )
         }
@@ -158,7 +179,6 @@ class Stickers extends Component {
                 <Row>
                     <Col xs="12">
                         {comp}
-                        
                     </Col>
                 </Row>
                                                                                   
